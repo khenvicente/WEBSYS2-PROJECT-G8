532412a90548c8b3507f23e29b9c08902df2700c
Khen <khenvicente246@gmail.com>
2025-12-11 21:40:49 +0800

    Updated db

--- express-ts/src/models/index.ts
+++ express-ts/src/models/index.ts
@@ -9,6 +9,12 @@ export const sequelize = new Sequelize(process.env.DATABASE_URL || '', {
   dialect: 'postgres',
   protocol: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 // export const sequelize = new Sequelize(
 //   process.env.DB_NAME || 'famili_dev',
